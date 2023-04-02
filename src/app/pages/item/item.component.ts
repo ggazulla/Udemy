c194b868ce85805e7653ab7331ffb4714fbb6933
Guillem Gazulla <guillem.gazulla@gmail.com>
2023-04-02 23:07:30 +0200

    Search added

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductosService } from 'src/app/services/productos.service';
+import { ItemDescription } from 'src/app/interfaces/item-description.interface';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent {
+export class ItemComponent implements OnInit{
 
+  producto: ItemDescription | undefined;
+  id: string | undefined;
+
+  constructor ( private route: ActivatedRoute,
+                public _itemService: ProductosService ){ }
+
+  ngOnInit(){
+    this.route.params.subscribe(parametros => {
+      // console.log(parametros['id']);
+      this._itemService.getProducto(parametros['id'])
+        .subscribe( (producto: ItemDescription) => {
+          // console.log(producto);
+          this.id = parametros['id'];
+          this.producto = producto;
+        })
+
+    });
+  }
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -10,17 +10,57 @@ export class ProductosService {
 
   productos: ItemInterface[] = [];
 
+  productosFiltrado: ItemInterface[] = [];
+
   constructor( private http: HttpClient ) {
     this.cargarProductos();
   }
 
   private cargarProductos() {
-    this.http.get<ItemInterface[]>('https://udemy-95743-default-rtdb.europe-west1.firebasedatabase.app/productos_idx.json')
+
+    return new Promise((resolve, reject)=>{
+      this.http.get<ItemInterface[]>('https://udemy-95743-default-rtdb.europe-west1.firebasedatabase.app/productos_idx.json')
              .subscribe( resp => {
 
-              console.log(resp);
+              // console.log(resp);
               this.productos = resp;
               this.cargando = false;
+              resolve("Resolve successful");
             });
+    })
+  }
+
+  getProducto(id: string){
+    return this.http.get(`https://udemy-95743-default-rtdb.europe-west1.firebasedatabase.app/productos/${ id }.json`);
+  }
+
+  buscarProducto(termino: string){
+    if(this.productos.length === 0){
+      //cargar productos
+      this.cargarProductos().then(()=>{
+        //ejecutar despues de tener los productos
+        // aplicar filtro
+        this.filtrarProductos(termino);
+      });
+    }else{
+      // aplicar filtro
+      this.filtrarProductos(termino);
+    }
+    
+  }
+
+  private filtrarProductos(termino: string){
+    this.productosFiltrado = [];
+    termino = termino.toLocaleLowerCase();
+    this.productos.forEach( prod => {
+      const tituloLower = prod.titulo!.toLocaleLowerCase();
+      if(prod.categoria!.indexOf(termino) >= 0 || tituloLower.indexOf(termino) >= 0){
+        this.productosFiltrado.push(prod);
+      }
+    })
+    // this.productosFiltrado = this.productos.filter(producto => {
+    //   return true;
+    // });
+    // console.log(this.productosFiltrado);
   }
 }
